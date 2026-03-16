ce79d57124c4d5bc89d2e6ff211ebbd98594de48
Vishnu Prajapat <dev.vishnuprajapat@gmail.com>
2026-03-16 13:32:25 +0530

    Add CORS support to the application

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ import express from "express";
 import { todoRouter } from "./routes/todo.routes.js";
 import { userRouter } from "./routes/user.routes.js";
 import { sendError, sendSuccess } from "./utils/response.js";
-
+ import cors from "cors";
 const app = express();
 
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}
+));
 app.use(express.json());
 app.get("/", (req, res) =>
   sendSuccess(
